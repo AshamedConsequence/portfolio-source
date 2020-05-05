85679713be48056e3efee7ea8590865480b0e428
Tarek Ali <tarekali15@outlook.com>
2020-05-05 12:40:30 -0500

    changed text removed hyperlink

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -8,17 +8,9 @@ function App() {
       <header className="App-header">
         <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" />
         <br></br>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          DGKSK8LIFE's Portfolio
+        </h2>
       </header>
     </div>
   );

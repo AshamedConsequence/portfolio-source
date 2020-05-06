d08d946bfabad57d53a447ca5690c202bea0aa5a
Tarek Ali <tarekali15@outlook.com>
2020-05-05 20:48:19 -0500

    added lazy loading of the image

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" />
+        <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
         <br></br>
         <h2>
           <i>Tarek Ali (DGKSK8LIFE)</i>

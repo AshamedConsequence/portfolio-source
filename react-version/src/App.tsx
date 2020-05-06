ac1f6b53127d8b17991acb665d44cb02900409df
Tarek Ali <tarekali15@outlook.com>
2020-05-06 14:01:15 -0500

    added react fragments unstable**

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -2,37 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Nav } from 'react-bootstrap';
+import { render } from 'react-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Nav variant="pills" defaultActiveKey="/home">
-          <Nav.Item>
-            <Nav.Link href="/home">Active</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
+    <React.Fragment>
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
     </Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
-        <h2>
-          <i>Tarek Ali (DGKSK8LIFE)</i>
-        </h2>
-        <footer className="frontpage_footer">
-          <a href="https://github.com/DGKSK8LIFE/"><img src="icons8-github.svg" className="github"></img></a>
+        </Nav.Item>
+      </Nav>
+    </React.Fragment>
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
+          <h2>
+            <i>Tarek Ali (DGKSK8LIFE)</i>
+          </h2>
+          <footer className="frontpage_footer">
+            <a href="https://github.com/DGKSK8LIFE/"><img src="icons8-github.svg" className="github"></img></a>
           &nbsp;
           &nbsp;
           &nbsp;
           <a href="mailto:tarekali15@outlook.com"><img src="interface.svg" className="mail"></img></a>
-        </footer>
-      </header>
-    </div>
-  );
+          </footer>
+        </header>
+      </div>
+    </React.Fragment>
+  )
 }
+
 

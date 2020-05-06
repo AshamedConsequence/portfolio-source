5154abf340aba12084a53f1222c77017e4aed2bd
Tarek Ali <tarekali15@outlook.com>
2020-05-06 10:45:16 -0500

    removed navbar; gonna use material-uis

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Navbar>
         <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
         <h2>
           <i>Tarek Ali (DGKSK8LIFE)</i>

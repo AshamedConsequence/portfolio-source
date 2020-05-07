27940e72afc8f3fb7f212fbcc65e167e43553729
Tarek Ali <tarekali15@outlook.com>
2020-05-07 14:42:04 -0500

    made Nav.tsx; imported and used it; working on gettting it to work

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
+import Fragment from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Nav } from 'react-bootstrap';
 import { render } from 'react-dom';
+import { NavBar } from './Nav';
 
 export default function App() {
   return (
-    <React.Fragment>
-      <Nav variant="tabs" defaultActiveKey="/">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="{}">About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="{}">Projects</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </React.Fragment>
-    <React.Fragment>
+    <Fragment>
+      <NavBar />
       <div className="App">
         <header className="App-header">
           <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
@@ -35,8 +25,8 @@ export default function App() {
           </footer>
         </header>
       </div>
-    </React.Fragment>
-  );
+    </Fragment>
+  )
 }
 
 
---
+++ react-version/src/Nav.tsx
@@ -0,0 +1,17 @@
+import { Button, Nav } from 'react-bootstrap';
+
+export default function NavBar() {
+    return (
+        <Nav variant="tabs" defaultActiveKey="/">
+            <Nav.Item>
+                <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="{}">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="{}">Projects</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    )
+}

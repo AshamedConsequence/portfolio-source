a312598c0439a0f8aaa5e1896dcddf58b749d1b6
Tarek Ali <tarekali15@outlook.com>
2020-05-08 07:27:48 -0500

    added divs

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -10,7 +10,9 @@ export default class App extends React.Component<any, any> {
   render() {
     return (
       <Fragment>
-        <NavBar />
+        <div>
+          <NavBar />
+        </div>
         <div className="App">
           <header className="App-header">
             <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />

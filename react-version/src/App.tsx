213ff17cfb57e9d72955a42a87bdb91a68ab7367
Tarek Ali <tarekali15@outlook.com>
2020-05-08 09:12:17 -0500

    removed navbar; going to use materialui

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -9,34 +9,21 @@ import { render } from 'react-dom';
 export default class App extends React.Component<any, any> {
   render() {
     return (
-      <Fragment>
-        <ul className="list-reset flex">
-          <li className="mr-3">
-            <a className="inline-block border border-blue rounded py-1 px-3 bg-blue text-white" href="#">Active Pill</a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block border border-white rounded hover:border-grey-lighter text-blue hover:bg-grey-lighter py-1 px-3" href="#">Pill</a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block py-1 px-3 text-grey-light cursor-not-allowed" href="#">Disabled Pill</a>
-          </li>
-        </ul>
-        <div className="App">
-          <header className="App-header">
-            <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
-            <h1>
-              <i>Tarek Ali (DGKSK8LIFE)</i>
-            </h1>
-            <footer className="frontpage_footer">
-              <a href="https://github.com/DGKSK8LIFE/"><img src="icons8-github.svg" className="github"></img></a>
+      <div className="App">
+        <header className="App-header">
+          <img src="https://avatars2.githubusercontent.com/u/45473363?s=460&u=76fc783175bccafd8c99abdf1d69cb86a6002372&v=4" className="App-logo" alt="logo" loading="lazy" />
+          <h1>
+            <i>Tarek Ali (DGKSK8LIFE)</i>
+          </h1>
+          <footer className="frontpage_footer">
+            <a href="https://github.com/DGKSK8LIFE/"><img src="icons8-github.svg" className="github"></img></a>
           &nbsp;
           &nbsp;
           &nbsp;
           <a href="mailto:tarekali15@outlook.com"><img src="interface.svg" className="mail"></img></a>
-            </footer>
-          </header>
-        </div>
-      </Fragment>
+          </footer>
+        </header>
+      </div>
     )
   }
 }

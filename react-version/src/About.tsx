c618641952d3f0bf7b4cd407f6d00231711fb349
Tarek Ali <tarekali15@outlook.com>
2020-05-12 14:15:19 -0500

    redesigned about page

--- react-version/src/About.tsx
+++ react-version/src/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-export default class App extends React.Component<any, any> {
+export default class About extends React.Component<any, any> {
   render() {
     return (
       <div className="App">
@@ -9,7 +9,16 @@ export default class App extends React.Component<any, any> {
           <h1>
             <i>About</i>
           </h1>
-          <p></p>
+            <p>I'm very interested in <strong>server-side</strong> development and <strong>webservices/relational datastructures/informational security</strong>. I know 
+             <strong> go</strong>, <strong>js</strong>, <strong>python</strong>, <strong>java</strong>, and
+            <strong>html/css</strong>.
+            I've also
+            written a few hundred
+            lines of <strong>C/C++</strong>. I enjoy Vex and FRC Robotics (I'm on team <strong>6785M and 5829</strong>).
+            I'm 14;
+            currently living in Houston, TX. For more information, <a
+                href='https://github.com/DGKSK8LIFE/WIKNWINTL/blob/master/README.md'>Click Here.</a> I am a native English speaker, and am fluent/semi-fluent in French. Arabic is 
+                my third language; I can read and write it in but definitely need some improvement. I am also learning Russian (self-taught).</p>
           <footer className="frontpage_footer">
             <a href="https://github.com/DGKSK8LIFE/"><img src="icons8-github.svg" className="github"></img></a>
           &nbsp;

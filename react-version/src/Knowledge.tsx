8e284057c385737dd668b99cd5efec173e31e10b
Tarek Ali <tarekali15@outlook.com>
2020-05-13 13:24:54 -0500

    added knowledge page content and LIs; going to change list items to icons soon

--- react-version/src/Knowledge.tsx
+++ react-version/src/Knowledge.tsx
@@ -9,6 +9,28 @@ export default class Knowledge extends React.Component<any, any> {
           <h1>
             <i>Knowledge</i>
           </h1>
+          <div className="Knowledge-headers">
+            <h3>Backend</h3>
+            <ul>
+              <li>Go</li> 
+              <li>Python</li>
+              <li>Node</li>
+              <li>Java</li>
+            </ul>
+            <h3>Frontend</h3>
+            <ul>
+            <li>HTML 5</li>
+            <li>CSS 3</li>
+            <li>Javascript</li>
+            <li>React</li>
+            </ul>
+            <h3>Database technologies</h3>
+            <ul>
+              <li>MySQL</li>
+              <li>Sqlite</li>
+            </ul>
+
+          </div>
         </header>
       </div>
     )

18be1f84b82e00132c839d44f5da54cec141c2bb
Tarek Ali <tarekali15@outlook.com>
2020-05-13 08:52:26 -0500

    removed whitespace

--- react-version/src/Certs.tsx
+++ react-version/src/Certs.tsx
@@ -9,7 +9,6 @@ export default class Certs extends React.Component<any, any> {
           <h1>
             <i>Badges/Certifications</i>
           </h1>
-
           <img src="https://www.awseducate.com/mstudent/resource/Minor_Badge_Icons/BuilderBadgeComplete.png"></img>
         </header>
       </div>

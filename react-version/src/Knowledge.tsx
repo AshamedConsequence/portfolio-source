f92dbd79b19b6ff42efefaeb6ec8313ae9ef9c10
Tarek Ali <tarekali15@outlook.com>
2020-05-13 13:29:40 -0500

    added items to list

--- react-version/src/Knowledge.tsx
+++ react-version/src/Knowledge.tsx
@@ -16,12 +16,15 @@ export default class Knowledge extends React.Component<any, any> {
               <li>Python</li>
               <li>Node</li>
               <li>Java</li>
+              <li>Flask</li>
+              <li>Gin router</li>
+              <li>Revel framework</li>
             </ul>
             <h3>Frontend</h3>
             <ul>
             <li>HTML 5</li>
             <li>CSS 3</li>
-            <li>Javascript</li>
+            <li>Javascript/Typescript</li>
             <li>React</li>
             </ul>
             <h3>Database technologies</h3>
@@ -29,7 +32,6 @@ export default class Knowledge extends React.Component<any, any> {
               <li>MySQL</li>
               <li>Sqlite</li>
             </ul>
-
           </div>
         </header>
       </div>

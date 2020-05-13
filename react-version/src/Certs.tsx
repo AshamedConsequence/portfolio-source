3c7ded7b0a2d47d834ac469657ae36bff531a002
Tarek Ali <tarekali15@outlook.com>
2020-05-13 15:33:45 -0500

    breakline

--- react-version/src/Certs.tsx
+++ react-version/src/Certs.tsx
@@ -9,6 +9,7 @@ export default class Certs extends React.Component<any, any> {
             <h1>
                 <i>Badges/Certifications</i>
             </h1>
+            <br/>
             <div className="card">
                 <div className="container">
                     <img src="https://www.awseducate.com/mstudent/resource/Minor_Badge_Icons/BuilderBadgeComplete.png"></img>

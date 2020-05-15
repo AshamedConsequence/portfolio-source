f4edf3b79421d64ba6d16b0d4649eb71819d356e
Tarek Ali <tarekali15@outlook.com>
2020-05-14 22:04:45 -0500

    removed shit from .gitignore

---
+++ react-version/build/precache-manifest.1da84cb74d491ca16fed2158c97e9c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8dbabbcecf92ce3a297db1ef0e7288a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54772da955e80537c2c",
+    "url": "/static/css/main.cecb0ceb.chunk.css"
+  },
+  {
+    "revision": "449c3b5fd163141e400f",
+    "url": "/static/js/2.105b6cd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.105b6cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54772da955e80537c2c",
+    "url": "/static/js/main.99352b0b.chunk.js"
+  },
+  {
+    "revision": "7c9d47fd73e17ab2e6dd",
+    "url": "/static/js/runtime-main.f291cd3a.js"
+  }
+]);

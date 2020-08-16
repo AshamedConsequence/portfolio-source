c27cad05c4669d2ac9893ae046100cc5e59d9c34
Tarek Ali <tarekali15@outlook.com>
2020-08-16 09:29:52 -0500

    updated some info

--- react-version/src/About.tsx
+++ react-version/src/About.tsx
@@ -22,14 +22,14 @@ export default class About extends React.Component<any, any> {
             <i>About</i>
           </h1>
             <p className="about-block">I'm very interested in <strong>server-side</strong> development and <strong>webservices/relational datastructures/informational security</strong>. I know 
-             <strong> go</strong>, <strong>js</strong>, <strong>python</strong>, <strong>java</strong>, and
-            <strong> html/css</strong>.
+             <strong>go</strong>, <strong>js</strong>, <strong>python</strong>, <strong>java</strong>
+            <strong>html/css</strong>, and <strong>C</strong>.
             I've also
             written a few hundred
             lines of <strong>C/C++</strong>. I enjoy Vex and FRC Robotics (I'm on team <strong>6785M and 5829</strong>).
             I'm 14;
             currently living in Houston, TX. For more information about my computer science knowledge, <a
-                href='https://github.com/DGKSK8LIFE/WIKNWINTL/blob/master/README.md' className="wiknwintl">Click Here.</a> I am a native English speaker, and am fluent/semi-fluent in French. Arabic is 
+                href='https://github.com/DGKSK8LIFE/WIKNWINTL/blob/master/README.md' className="wiknwintl">Click Here.</a> I am a native English speaker, and am fluent in French. Arabic is 
                 my third language; I am literate in it but definitely need some improvement. I am also learning Russian (self-taught).</p>
         </header>
       </div>

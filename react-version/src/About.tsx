4767cbd25b1971429822709ff358cf44d2e2e95f
Tarek Ali <tarekali15@outlook.com>
2020-08-16 09:35:25 -0500

    changed some info

--- react-version/src/About.tsx
+++ react-version/src/About.tsx
@@ -21,12 +21,11 @@ export default class About extends React.Component<any, any> {
           <h1>
             <i>About</i>
           </h1>
-            <p className="about-block">I'm very interested in <strong>server-side</strong> development and <strong>webservices/relational datastructures/informational security</strong>. I know 
-             <strong>go</strong>, <strong>js</strong>, <strong>python</strong>, <strong>java</strong>
+            <p className="about-block">I'm very interested in <strong>server-side development, webservices, relational datastructures, informational security, and more!</strong>. I know 
+             <strong> go</strong>, <strong>js</strong>, <strong>python</strong>, <strong>java </strong>
             <strong>html/css</strong>, and <strong>C</strong>.
-            I've also
-            written a few hundred
-            lines of <strong>C/C++</strong>. I enjoy Vex and FRC Robotics (I'm on team <strong>6785M and 5829</strong>).
+            I've also written some
+            <strong> C++</strong>. I participate in Vex and FRC Robotics (team <strong>6785M and 5829</strong>).
             I'm 14;
             currently living in Houston, TX. For more information about my computer science knowledge, <a
                 href='https://github.com/DGKSK8LIFE/WIKNWINTL/blob/master/README.md' className="wiknwintl">Click Here.</a> I am a native English speaker, and am fluent in French. Arabic is 
